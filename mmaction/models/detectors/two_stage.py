09a985d9d66df6eb4a5a6b0e6cf06d0dcd7e39b0
Yinghao Xu <justimyhxu@zju.edu.cn>
2019-08-16 22:58:30 +0800

    update

--- mmaction/models/detectors/two_stage.py
+++ mmaction/models/detectors/two_stage.py
@@ -93,10 +93,8 @@ class TwoStageDetector(BaseDetector, RPNTestMixin, BBoxTestMixin):
 
         losses = dict()
 
-
         # RPN forward and loss
         if self.with_rpn:
-
             x_slice = (xx[:, :, xx.size(2) // 2, :, :] for xx in x)
             rpn_outs = self.rpn_head(x_slice)
             rpn_loss_inputs = rpn_outs + \
